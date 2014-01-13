beb0982eb6e09e16ba9fe8108be0655b2bde2aac
David Bordoley <bordoley@gmail.com>
2014-01-12 22:24:31 -0800

    update documentation

--- docs.dart
+++ docs.dart
@@ -1,12 +1,12 @@
 import "package:restlib_common/collections.dart";
 import "package:restlib_common/objects.dart";
 import "package:restlib_common/preconditions.dart";
+import "package:restlib_http_connector/connector.dart";
 import "package:restlib_core/data.dart";
 import "package:restlib_core/http.dart";
 import "package:restlib_core/http_syntax.dart";
 import "package:restlib_core/multipart.dart";
 import "package:restlib_parsing/parsing.dart";
-import "package:restlib_server/connector.dart";
 import "package:restlib_server/io.dart";
 import "package:restlib_server/server.dart";
 import "package:restlib_testing/testing.dart";
@@ -18,12 +18,12 @@ void main() {
       ["restlib.common.collections",
        "restlib.common.objects",
        "restlib.common.preconditions",
+       "restlib.connector.http",
        "restlib.core.data",
        "restlib.core.http",
        "restlib.core.http_syntax",
        "restlib.core.multipart",
        "restlib.parsing",
-       "restlib.server.connector",
        "restlib.server.io",
        "restlib.server",
        "restlib.testing"];
@@ -31,6 +31,8 @@ void main() {
   final List<String> arguments =
       ["--package-root", "./packages", "--include-lib", "${libs.join(",")}", "docs.dart"];
   final String path = Platform.executable.substring(0, Platform.executable.lastIndexOf("/")) + "/dartdoc";
+  
+  
   
   Process.run(path, arguments)
     .then((final ProcessResult r) => 
