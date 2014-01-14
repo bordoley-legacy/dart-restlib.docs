a09045f944f3370ee9b5c3ece4c297566ce17787
David Bordoley <bordoley@gmail.com>
2014-01-14 09:11:32 -0800

    update docs

--- docs.dart
+++ docs.dart
@@ -1,4 +1,6 @@
+import "package:restlib_common/async.dart";
 import "package:restlib_common/collections.dart";
+import "package:restlib_common/io.dart";
 import "package:restlib_common/objects.dart";
 import "package:restlib_common/preconditions.dart";
 import "package:restlib_http_connector/connector.dart";
@@ -11,11 +13,14 @@ import "package:restlib_server/io.dart";
 import "package:restlib_server/server.dart";
 import "package:restlib_testing/testing.dart";
 
+import "dart:async";
 import "dart:io";
 
 void main() {
   final Iterable<String> libs = 
-      ["restlib.common.collections",
+      ["restlib.common.async",
+       "restlib.common.collections",
+       "restlib.common.io",
        "restlib.common.objects",
        "restlib.common.preconditions",
        "restlib.connector.http",
@@ -29,10 +34,8 @@ void main() {
        "restlib.testing"];
   
   final List<String> arguments =
-      ["--package-root", "./packages", "--include-lib", "${libs.join(",")}", "docs.dart"];
+      ["--omit-generation-time", "--package-root", "./packages", "--include-lib", "${libs.join(",")}", "docs.dart"];
   final String path = Platform.executable.substring(0, Platform.executable.lastIndexOf("/")) + "/dartdoc";
-  
-  
   
   Process.run(path, arguments)
     .then((final ProcessResult r) => 
